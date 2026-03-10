dca181cefa8e3aa95f0350c35677f35f48518c2e
Chris Geyer <chris@treqs.ai>
2026-03-10 18:33:03 +0000

    default value for chunk

--- src/produce.py
+++ src/produce.py
@@ -8,7 +8,7 @@ import os
 def main() -> int:
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--seed", required=True, type=int, help="Random seed")
-    parser.add_argument("--chunk", required=True, type=int, help="Chunk size")
+    parser.add_argument("--chunk", required=False, default=0, type=int, help="Chunk size")
     parser.add_argument("--n", required=True, type=int, help="Number of floats")
     parser.add_argument("filename", help="Output filename")
     args = parser.parse_args()
@@ -16,6 +16,9 @@ def main() -> int:
     if args.n < 0:
         parser.error("n must be non-negative")
 
+    if args.chunk <= 0:
+        chunk = n
+        
     random.seed(args.seed)
 
     chunk = args.chunk
